ae5bafaaf7ba30ef51636e1a3eea46015297563c
I7RANK <franfgh2000@hotmail.com>
2025-08-28 16:06:54 -0500

    feat: move form to component and add basic styles

--- src/app/create-store/page.tsx
+++ src/app/create-store/page.tsx
@@ -1,74 +1,11 @@
-'use client';
-
-import { useState } from 'react';
+import CreateStoreForm from "@/components/CreateStoreForm";
 
 export default function CreateStore() {
-  const [form, setForm] = useState({ name: '', address: '', city: '' });
-  const cities = ['Barranquilla', 'Bogota', 'Cali'];
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(process.env.NEXT_PUBLIC_API_URL);
-
-    try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/stores`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(form),
-      });
-      console.log(res);
-    } catch (error) {
-      console.error(error);
-    }
-  };
   return (
-    <div className="bg-amber-50 flex flex-col items-center">
-      <h1>Añadir nueva tienda</h1>
-      <section>
-        <form onSubmit={handleSubmit}>
-          <label className="" htmlFor="name">
-            Nombre de la tienda:{' '}
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={form.name}
-            onChange={(e) => setForm({ ...form, name: e.target.value })}
-          />
-          Dirección:{' '}
-          <input
-            type="text"
-            value={form.address}
-            onChange={(e) => setForm({ ...form, address: e.target.value })}
-          />
-          <fieldset>
-            <legend>Ciudad:</legend>
-            {cities.map((city) => {
-              return (
-                <div key={city}>
-                  <input
-                    type="radio"
-                    name="city"
-                    id={city}
-                    value={city}
-                    checked={form.city === city}
-                    onChange={(e) => setForm({ ...form, city: e.target.value })}
-                  />
-                  <label htmlFor={city}>{city}</label>
-                </div>
-              );
-            })}
-          </fieldset>
-          <button
-            className="bg-blue-400 px-4 py-2 rounded-md shadow"
-            type="submit"
-          >
-            Crear Tienda
-          </button>
-        </form>
+    <div className="flex flex-col items-center bg-amber-50 pt-10">
+      <section className="relative w-full">
+        <CreateStoreForm />
       </section>
-      {JSON.stringify(form, null, 2)}
     </div>
   );
 }
---
+++ src/components/CreateStoreForm.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
+export default function CreateStoreForm() {
+  const [form, setForm] = useState({ name: "", address: "", city: "" });
+  const [isLoading, setIsLoading] = useState(false);
+  const cities = ["Barranquilla", "Bogota", "Cali"];
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/stores`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+      if (!res.ok) {
+        const errorBody = await res.json().catch(() => null);
+        throw new Error(
+          `Request failed with status ${res.status} - ${res.statusText} | ${JSON.stringify(errorBody)}`,
+        );
+      }
+      resetForm();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const resetForm = async () => {
+    setForm({ name: "", address: "", city: "" });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1 className="text-lg font-bold">Añadir nueva tienda</h1>
+      <p className="mt-1 text-sm text-gray-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        explicabo aliquid culpa vitae fugiat? Quaerat tenetur
+      </p>
+      <div className="mt-2 flex flex-col gap-y-2">
+        <div>
+          <label htmlFor="name">Nombre de la tienda: </label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Frisby"
+            value={form.name}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="address">Dirección: </label>
+          <input
+            id="address"
+            type="text"
+            placeholder="Cll. 98 # 51b - 55"
+            value={form.address}
+            onChange={(e) => setForm({ ...form, address: e.target.value })}
+          />
+        </div>
+        <fieldset>
+          <legend>Ciudad:</legend>
+          {cities.map((city) => {
+            return (
+              <div key={city}>
+                <input
+                  type="radio"
+                  name="city"
+                  id={city}
+                  value={city}
+                  checked={form.city === city}
+                  onChange={(e) => setForm({ ...form, city: e.target.value })}
+                />
+                <label className="ml-1 font-normal" htmlFor={city}>
+                  {city}
+                </label>
+              </div>
+            );
+          })}
+        </fieldset>
+      </div>
+
+      <button
+        className="mt-4 flex items-center justify-center rounded-md bg-sky-400 px-4 py-2 shadow disabled:bg-gray-200 disabled:inset-shadow-sm"
+        disabled={isLoading}
+        type="submit"
+      >
+        {isLoading ? (
+          <Image
+            className="animate-spin"
+            src="/spinner.svg"
+            alt="spinner"
+            width={24}
+            height={24}
+          />
+        ) : (
+          "Añadir Tienda"
+        )}
+      </button>
+      {JSON.stringify(form, null, 2)}
+    </form>
+  );
+}
