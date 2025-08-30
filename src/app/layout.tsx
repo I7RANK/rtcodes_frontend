2b874e54719c2d81a8460fde94319f3dc49fbe37
I7RANK <franfgh2000@hotmail.com>
2025-08-30 10:39:12 -0500

    feat: add footer and request to initializate back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,23 +18,29 @@ export const metadata: Metadata = {
   description: "Let's get the party started",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ping`);
+  console.log(`(\x1b[32m${res.status}\x1b[0m) ${await res.text()}`);
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} relative antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} relative flex min-h-screen flex-col antialiased`}
       >
-        <Link href="/">
-          <button className="mt-3 ml-3 rounded-sm bg-gray-100 px-4 py-2 text-sm">
-            Inicio
-          </button>
-        </Link>
-        {children}
-        <div className="absolute top-0 -z-10 h-52 w-full bg-linear-to-br from-sky-800 to-sky-950"></div>
+        <main className="flex-1">
+          <Link href="/">
+            <button className="mt-3 ml-3 rounded-sm bg-gray-100 px-4 py-2 text-sm">
+              Inicio
+            </button>
+          </Link>
+          {children}
+          <div className="absolute top-0 -z-10 h-52 w-full bg-linear-to-br from-sky-800 to-sky-950"></div>
+        </main>
+
+        <footer className="mt-10 h-20 w-full bg-linear-to-br from-sky-950 to-sky-800"></footer>
       </body>
     </html>
   );
